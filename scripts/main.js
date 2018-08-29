0982b9553d2d68714d4f530d4e2c8c40b8bee80b
Shih Han <shih-han.wang@boxt.co.uk>
2018-08-29 12:10:33 +0100

    add/header/order/inventory calsses added

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,53 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+// App
+
+var App = React.createClass({
+
+  render: function(){
+     return(
+       <div className="catch-of-the-day">
+         <div className="menu">
+           <Header />
+         </div>
+         <Order/>
+         <Inventory/>
+       </div>
+     )
+  }
+});
+
+// Header
+
+var Header = React.createClass({
+  render: function(){
+     return(
+       <p>Header</p>
+     )
+  }
+});
+
+// Order
+
+var Order = React.createClass({
+  render: function(){
+     return(
+       <p>Order</p>
+     )
+  }
+});
+
+// Inventory
+
+var Inventory = React.createClass({
+  render: function(){
+     return(
+       <p>Inventory</p>
+     )
+  }
+});
+
 // StorePicker
 
 var StorePicker = React.createClass({
@@ -16,4 +63,4 @@ var StorePicker = React.createClass({
   }
 });
 
-ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
+ReactDOM.render(<App/>, document.querySelector('#main'));
