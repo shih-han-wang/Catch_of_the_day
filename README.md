b4e5b4335b2dce16293ded1db7f21c1f797f84a4
Shih Han <shih-han.wang@boxt.co.uk>
2018-08-29 11:54:54 +0100

    set up

---
+++ README.md
@@ -0,0 +1,3 @@
+# Catch_of_the_day
+
+ReactJS application built in this [React For Beginners](https://github.com/wesbos/React-For-Beginners-Starter-Files) course.
--- scripts/components/NotFound.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export class NotFound extends React.Component {
-  render() {
-    return (
-      <h1>Not Found!</h1>
-    )
-  }
-};
